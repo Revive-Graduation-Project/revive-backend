37c0d503cf338215735171cb7a1661e8afa0de5b
Abdulrahman Al-Sayed <haniabood935@gmail.com>
2026-06-30 15:57:32 +0300

    add cancelation ticket logic

---
+++ kitchen-service/src/main/java/com/restaurant/kitchen/events/OrderCanceledEvent.java
@@ -0,0 +1,4 @@
+package com.restaurant.kitchen.events;
+
+public class OrderCanceledEvent {
+}
---
+++ kitchen-service/src/main/java/com/restaurant/kitchen/events/ticketEvents/TicketCanceledFailedEvent.java
@@ -0,0 +1,4 @@
+package com.restaurant.kitchen.events.ticketEvents;
+
+public class TicketCanceledFailedEvent {
+}
